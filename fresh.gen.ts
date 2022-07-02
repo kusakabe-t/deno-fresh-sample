869c07be356e2ad08bd80055138514f370c31a75
kusakabe <t.kusakabe0429@gmail.com>
2022-07-03 00:40:42 +0900

    add fresh.gen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,19 +2,29 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
+import * as $0 from "./routes/about.tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $2 from "./routes/counter.tsx";
+import * as $3 from "./routes/github/[username].tsx";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/names/[name].tsx";
+import * as $6 from "./routes/search.tsx";
+import * as $$0 from "./islands/Countdown.tsx";
+import * as $$1 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
+    "./routes/about.tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/counter.tsx": $2,
+    "./routes/github/[username].tsx": $3,
+    "./routes/index.tsx": $4,
+    "./routes/names/[name].tsx": $5,
+    "./routes/search.tsx": $6,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/Countdown.tsx": $$0,
+    "./islands/Counter.tsx": $$1,
   },
   baseUrl: import.meta.url,
 };
